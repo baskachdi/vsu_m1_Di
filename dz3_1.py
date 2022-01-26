12e58d72140a08e9aa16446c7e12d4983cbb74fc
baskachdi <90783128+baskachdi@users.noreply.github.com>
2022-01-26 09:22:46 +0300

    Add files via upload

--- dz3_1.py
+++ dz3_1.py
@@ -1,13 +1,13 @@
 class Fract:
-    def __init__ (self, num = 0, denom = 0):
+    def __init__(self, num = 0, denom = 0):
         self.num = num
         self.denom = denom
     def inputfract (self):
         self.num = int(input("Введите числитель: "))
-        self.denom = int(input("Введите знаменатель: "))
-    def printfract (self):
-        print(self.num,"/",self.denom)
-        
+        while (self.denom == 0):
+            self.denom = int(input("Введите знаменатель: "))
+            if (self.denom == 0):
+               print("знаменатель равный нулю недопустим.")
 fract = Fract()
 fract.inputfract()
 fract.printfract()
